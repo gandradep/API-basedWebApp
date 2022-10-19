dbb0442927f858b492218c2938300fbe98870d06
gandradep <eventsonido@gmail.com>
2022-10-19 11:37:36 +0100

    creating obj with the data needed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import './style.css';
 import logo from './pokeball.png';
 
-import { getPokemon } from './modules/getPokemon.js';
+import { buildObj } from './modules/getPokemon.js';
 
 import { display } from './modules/homeDisplay.js';
-
-
-
-getPokemon().then((json) =>display(json.results));
+buildObj();
--- src/modules/getPokemon.js
+++ src/modules/getPokemon.js
@@ -1,2 +1,29 @@
-export const getPokemon = () => fetch('https://pokeapi.co/api/v2/pokemon')
-  .then( async (response) => await response.json());
+
+const getPokemon = () => fetch('https://pokeapi.co/api/v2/pokemon')
+.then((response) => response.json())
+.then( async (json) => {
+  const resPromise = await json;
+  return resPromise.results;
+});
+
+export const buildObj = async () => {
+  const imgUrl = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/';
+  let indexImg;
+  const result = await getPokemon();
+  let newObjArr = [];
+  let count = 1;
+
+  result.forEach((item) => {
+    indexImg = count.toString().padStart(3, '0');
+    const responseObj = {
+      id: count,
+      imgSrc: `${imgUrl}${indexImg}.png`,
+      ...item,
+    };
+    newObjArr.push(responseObj);
+
+    count ++;
+  });
+  console.log(newObjArr);
+  return newObjArr;
+}
