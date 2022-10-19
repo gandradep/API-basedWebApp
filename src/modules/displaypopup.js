44b5814b9ae3e0362e56f82f1266b0a894c8b63b
tashisrael <tashisrael04@gmail.com>
2022-10-19 14:54:15 +0300

    Basic styling of popup

--- src/modules/displaypopup.js
+++ src/modules/displaypopup.js
@@ -1,34 +1,41 @@
 export const display =(data) => {
   const popup = document.querySelector('.popup-window');
-  const img = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png';
+  const img = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png';
   popup.innerText = '';
   const div3 = document.createElement('div');
-  div3.innerHTML += `<img src="${img}">`
-  div3.innerHTML += `<h2>Name:</h2><p>${data.name}</p>`
-  popup.appendChild(div3);
-  div3.innerHTML += `<p>Height: ${data.height} m</p>`
-  popup.appendChild(div3);
-
-  div3.innerHTML += `<p>Weight: ${data.weight/10} kgs</p>`
+  div3.className = "popcontainer";
+  div3.innerHTML += `<img class="img" src="${img}">`
+  div3.innerHTML += `<p class="name">${data.name}</p>`
   popup.appendChild(div3);
 
+  const div4 = document.createElement('div');
+  div4.className = "popcontainer1";
+  div4.innerHTML += `<p class="size">Height: ${data.height} m</p>`
+  popup.appendChild(div4);
+  div4.innerHTML += `<p class="size">Weight: ${data.weight/10} kgs</p>`
+  popup.appendChild(div4);
+
+  const lowerdiv = document.createElement('div')
+  lowerdiv.className = "lowerdiv";
   const div2 = document.createElement('div');
-  div2.innerHTML += `<h2>Abilities:</h2>`
-  popup.appendChild(div2);
+  div2.className = "popcontainer2";
+  div2.innerHTML += `<h2>Abilities</h2>`
   data.abilities.forEach((item) => {
     const div = document.createElement('ul');
     div.innerHTML += `<li>${item.ability.name}</li>`
-    popup.appendChild(div);
+    div2.appendChild(div);
+    lowerdiv.appendChild(div2);
   });
 
   const div1 = document.createElement('div');
-  div1.innerHTML += `<h2>Types:</h2>`
-  popup.appendChild(div1);
+  div1.className = "popcontainer3";
+  div1.innerHTML += `<h2>Types</h2>`
   data.types.forEach((item) => {
     const div = document.createElement('ul');
     div.innerHTML += `<li>${item.type.name}</li>`
-    popup.appendChild(div);
+    div1.appendChild(div);
+    lowerdiv.appendChild(div1);
   });
-  
+  popup.appendChild(lowerdiv);
 
 };
