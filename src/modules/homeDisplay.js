2b53902faa6ccccca02311b55b37b2b71c1e3344
gandradep <eventsonido@gmail.com>
2022-10-19 03:57:20 +0100

    homepage setup, add styling

--- src/modules/homeDisplay.js
+++ src/modules/homeDisplay.js
@@ -3,25 +3,33 @@ export const display = (data) => {
   const mainContainer = document.getElementById('main');
   let count = 1;
   let indexImg;
+  let numLikes = 2;
   const imgUrl = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/';
   mainContainer.innerText = '';
   data.forEach((item) => {
     // console.log(item.name);
-    // console.log(item.url);
     const div = document.createElement('div');
     const myImg = new Image();
+    const divNameLikes = document.createElement('div');
+    const name = document.createElement('span');
+    const likes = document.createElement('button');
+    const comment = document.createElement('button');
+    div.classList.add('dFlex', 'homeDivPokemon');
+    divNameLikes.classList.add('dFlex', 'divNameLikes');
+    /* url image link */
     indexImg = count.toString().padStart(3, '0');
-
     count ++;
-    console.log(indexImg);
     myImg.src = `${imgUrl}${indexImg}.png`;
-    // const name = document.createElement('span');
-    // const score = document.createElement('span');
-    // name.innerHTML = `${item.user} :`;
-    // name.classList.add('bold');
-    // score.innerHTML = item.score;
-    div.append(myImg);
-    // row.appendChild(td);
+    myImg.classList.add('homeImg');
+    likes.innerHTML = `<i class="fa-regular fa-heart rem15"></i> ${numLikes} likes`;
+    likes.classList.add('likes');
+    name.innerText = item.name.toUpperCase();
+    name.classList.add('rem15', 'fw500');
+    comment.innerText = 'Comments';
+    comment.classList.add('rem15');
+    divNameLikes.append(name, likes);
+    div.append(myImg, divNameLikes, comment);
+
     mainContainer.appendChild(div);
   });
 };
